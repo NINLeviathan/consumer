22fcde301c9a4b15244c4e6fc421ac4d9ec48c17
NINLeviathan <stig_r@yahoo.com>
2023-01-15 16:38:20 +0100

    Fix silly spelling

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char const *argv[])
 {
     std::cout << "I'm a consumer" << std::endl;
-    std::cout << "I am outputting the following lines from the modles that I consume:" << std::endl;
+    std::cout << "I am outputting the following lines from the modules that I consume:" << std::endl;
     return 0;
 }
